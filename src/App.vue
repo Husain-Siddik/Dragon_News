f41e3d5ea13a61549bb9b0916f957eab9a271f76
Husain-Siddik <ronysiddik21@gmail.com>
2024-08-28 22:37:55 +0600

    basic setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink :to="{name:'about'}">About</RouterLink>
+        <RouterLink :to= "{ name:'career' }">Career</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+
---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h class="text-5xl text-red-800">This is an about page</h>
+  </div>
+</template>
+<script setup>
+</script>
